f04981a097f90a10b14776f7076f1f6366389c0f
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2021-10-29 09:31:10 +0200

    :tada: project setup

---
+++ components/Phone.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Phone() {
+    return (
+        <div className={`phone`}>
+
+            Samuel Green
+            Available to Walk
+
+            That sounds great. I’d be happy with that.
+
+            Could you send over some pictures of your dog, please?
+
+            Here are a few pictures. She’s a happy girl!
+
+            Can you make it?
+
+            She looks so happy! The time we discussed works. How long shall I take her out for?
+
+            30 minute walk
+            $29
+
+            1 hour walk
+            $49
+
+            Type a message…
+        </div>
+    )
+}
+
+export default Phone
